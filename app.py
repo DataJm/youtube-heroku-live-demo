3ddf317f15699a892ecbc6dac2fc890c279f1bf5
datajm <ecojmbarrera@gmail.com>
2021-11-17 19:44:53 -0600

    update 1

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
+from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hola bootcamp"
+    return render_template("index.html")
+
+@app.route("/api_views")
+def api_views():
+    connection_string = "postgresql://vwjpswzrnmptct:f7d3585048d5d8fa64b2bdb0633aa8324f01e3d4ea044a37284ddcc918b9817a@ec2-52-201-195-11.compute-1.amazonaws.com:5432/dd9j1f8aub9l0u"
+    # connection_string = os.environ.get('DATABASE_URL', '')
+    conn = create_engine(connection_string)
+    data = pd.read_sql("select * from ratings",conn)
+
+    resultado = data.groupby("channel_title")["views","likes"].sum()
+
+    return (
+        resultado
+        .sort_values(by="views", ascending=False)
+        .reset_index()
+        .loc[:,["channel_title","views"]]
+        .head()
+        .to_json(orient="records")
+    )
+
+@app.route("/api_likes")
+def api_likes():
+    connection_string = "postgresql://vwjpswzrnmptct:f7d3585048d5d8fa64b2bdb0633aa8324f01e3d4ea044a37284ddcc918b9817a@ec2-52-201-195-11.compute-1.amazonaws.com:5432/dd9j1f8aub9l0u"
+    # connection_string = os.environ.get('DATABASE_URL', '')
+    conn = create_engine(connection_string)
+    data = pd.read_sql("select * from ratings",conn)
+
+    resultado = data.groupby("channel_title")["views","likes"].sum()
+
+    return (
+        resultado
+        .sort_values(by="likes", ascending=False)
+        .reset_index()
+        .loc[:,["channel_title","likes"]]
+        .head()
+        .to_json(orient="records")
+    )
+
+
 
 if __name__ == "__main__":
     app.run()
